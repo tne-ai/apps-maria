e1c7100b9f13355150de2d50d827ccf3fb2b7473
eligotts <78387377+eligotts@users.noreply.github.com>
2024-12-02 16:03:44 -0600

    Update merge_json.py

--- Code/merge_json.py
+++ Code/merge_json.py
@@ -3,35 +3,19 @@ import pandas as pd
 from tne.TNE import TNE
 from tabulate import tabulate
 import json
-'''
-if type(PROCESS_INPUT) is pd.DataFrame:
-    if PROCESS_INPUT.empty:
-        result = "<EMPTY DATAFRAME>"
-    else:
-        result = tabulate(PROCESS_INPUT, 
-                          headers="keys", 
-                          tablefmt="pipe", 
-                          showindex=False)
-elif type(PROCESS_INPUT) is str:
-    result = PROCESS_INPUT 
-else:
-    result = "<ERROR>" 
-'''
 
+session = TNE(uid=UID, bucket_name=BUCKET, project=PROJECT, version=VERSION)
+json_string = session.get_object('sec1_json.txt')
 
+document_filename = PROCESS_INPUT
 try:
     # Parse the input string as JSON
-    data = json.loads(PROCESS_INPUT)
+    data = json.loads(json_string)
     
-    # Extract chartData and tableData if they are present
-    output = {}
-    if "chartData" in data:
-        output["chartData"] = data["chartData"]
-    if "tableData" in data:
-        output["tableData"] = data["tableData"]
+    data['document_link_text'] = document_filename
     
     # Convert the result to a JSON string
-    result = json.dumps(output, indent=4)
+    result = json.dumps(data, indent=4)
 except Exception as e:
     # Capture the exception message in the result
     result = f"Error: {str(e)}"
