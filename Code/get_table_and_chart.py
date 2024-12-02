b34b6854ccc00cb2c686456bfe9ddeb32ea303d4
eligotts <78387377+eligotts@users.noreply.github.com>
2024-12-02 13:26:50 -0600

    Create get_table_and_chart.py

---
+++ Code/get_table_and_chart.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+from tne.TNE import TNE
+from tabulate import tabulate
+'''
+if type(PROCESS_INPUT) is pd.DataFrame:
+    if PROCESS_INPUT.empty:
+        result = "<EMPTY DATAFRAME>"
+    else:
+        result = tabulate(PROCESS_INPUT, 
+                          headers="keys", 
+                          tablefmt="pipe", 
+                          showindex=False)
+elif type(PROCESS_INPUT) is str:
+    result = PROCESS_INPUT 
+else:
+    result = "<ERROR>" 
+'''
+result = type(PROCESS_INPUT)
