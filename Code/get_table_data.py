de4968c3cc5031a2b2f981d22ea73aa257d90922
eligotts <78387377+eligotts@users.noreply.github.com>
2024-12-21 20:04:41 -0600

    Update get_table_data.py

--- Code/get_table_data.py
+++ Code/get_table_data.py
@@ -3,18 +3,30 @@ from tne.TNE import TNE
 
 try:
     # Parse the input string as JSON
-    for f in INPUT_FILES:
-        session = TNE(uid=UID, bucket_name=BUCKET, project=PROJECT, version=VERSION)
-        data = session.get_object(f)
-        data = json.loads(data)
+    # for f in INPUT_FILES:
+    #     session = TNE(uid=UID, bucket_name=BUCKET, project=PROJECT, version=VERSION)
+    #     data = session.get_object(f)
+    #     data = json.loads(data)
     
-        # Extract tableData if they are present
-        output = {}
-        if "tableData" in data:
-            output["tableData"] = data["tableData"]
+    #     # Extract tableData if they are present
+    #     output = {}
+    #     if "tableData" in data:
+    #         output["tableData"] = data["tableData"]
     
-        # Convert the result to a JSON string
-        result = json.dumps(output, indent=4)
+    #     # Convert the result to a JSON string
+    #     result = json.dumps(output, indent=4)
+    
+    session = TNE(uid=UID, bucket_name=BUCKET, project=PROJECT, version=VERSION)
+    data = session.get_object("final_json_no_chart.txt")
+    data = json.loads(data)
+
+    # Extract tableData if they are present
+    output = {}
+    if "tableData" in data:
+        output["tableData"] = data["tableData"]
+
+    # Convert the result to a JSON string
+    result = json.dumps(output, indent=4)
 except Exception as e:
     # Capture the exception message in the result
     result = f"Error: {str(e)}"
