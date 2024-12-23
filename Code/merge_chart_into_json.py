a6c20efbe72d860d31a16640f58ce72608cbd753
eligotts <78387377+eligotts@users.noreply.github.com>
2024-12-23 00:30:16 -0600

    Update merge_chart_into_json.py

--- Code/merge_chart_into_json.py
+++ Code/merge_chart_into_json.py
@@ -12,7 +12,7 @@ try:
     # Parse the input string as JSON
     data = json.loads(json_string)
     
-    data['chartData'] = json.loads(chart_string)
+    data['chartData'] = chart_string
     
     # Convert the result to a JSON string
     result = json.dumps(data, indent=4)
